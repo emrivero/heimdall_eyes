0ef3a4ada07faafa1fb2922933ba5575c374e46c
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2019-12-30 22:32:21 +0100

    New gh page deploy

---
+++ docs/precache-manifest.ab89fbb2e47616fa49ecb9df2e252aea.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492e0ab3d209316b5fadb4b69fd1a532",
+    "url": "https://emrivero.github.io/heimdall_eyes/index.html"
+  },
+  {
+    "revision": "e37fdf3de26233862eed",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/css/main.81c6b149.chunk.css"
+  },
+  {
+    "revision": "51239512614ae0900fca",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/js/2.d7ab1ac8.chunk.js"
+  },
+  {
+    "revision": "457601a85c8c817f4e2950577b4cd3a6",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/js/2.d7ab1ac8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e37fdf3de26233862eed",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/js/main.00752df1.chunk.js"
+  },
+  {
+    "revision": "c574b20449e6766fd3c8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/js/runtime-main.24bfd65b.js"
+  },
+  {
+    "revision": "149752c04764234f17b88a3d6d8d8c2f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/aliena.149752c0.png"
+  },
+  {
+    "revision": "dc31dbc365923e9fab5c402443b7edf0",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_azul.dc31dbc3.png"
+  },
+  {
+    "revision": "ac3b7551b57ee0e93d9a2710bf8eb424",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_enemigo_base.ac3b7551.png"
+  },
+  {
+    "revision": "0cf4da523eff95f77e58322c961366b7",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_enemigo_boss.0cf4da52.png"
+  },
+  {
+    "revision": "cbcf4011a848053db64294adce3ed228",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_enemigo_elite.cbcf4011.png"
+  },
+  {
+    "revision": "d3ad0b5a3409fd9450e37782917aab81",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_rojo.d3ad0b5a.png"
+  },
+  {
+    "revision": "27a0c6921adbba46f2db9ca0a0807703",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/banner_verde.27a0c692.png"
+  },
+  {
+    "revision": "24f10f2ebe24b531d65c500db5acc27c",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/base.24f10f2e.png"
+  },
+  {
+    "revision": "db50a70f06628395793bbfbbbff527f9",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/boss.db50a70f.png"
+  },
+  {
+    "revision": "d608a763414b7550c2d9fbf06d2004a8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/elba.d608a763.png"
+  },
+  {
+    "revision": "0e52328a5a390b506697f7f194b59d82",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/elite.0e52328a.png"
+  },
+  {
+    "revision": "8a6fc9c0a14ef6940aef00d485a1c439",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/flechas_inferior.8a6fc9c0.png"
+  },
+  {
+    "revision": "e1a469cf978423bc591a85471e912634",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/flechas_superior.e1a469cf.png"
+  },
+  {
+    "revision": "f4ee37038ce7072502b3d061b9927c8d",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/fondo.f4ee3703.jpg"
+  },
+  {
+    "revision": "506ffbc6d245558c45b2f417678d152f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/gema_verde.506ffbc6.png"
+  },
+  {
+    "revision": "4af1d9aeeca1d4ccec0243da7a9931d8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/gunar.4af1d9ae.png"
+  },
+  {
+    "revision": "d932a6e55f822e65b80522c9deaad40f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/level21.d932a6e5.png"
+  },
+  {
+    "revision": "11656010dc33805e3064528412799f70",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/level22.11656010.png"
+  },
+  {
+    "revision": "1078be560ed76e6d6731bdfb27f13fc2",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/level23.1078be56.png"
+  },
+  {
+    "revision": "049d7ba80fd979670af34f8cbd35e02d",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_1.049d7ba8.png"
+  },
+  {
+    "revision": "ead90a94102eb225b33c7ee5747de387",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_10.ead90a94.png"
+  },
+  {
+    "revision": "8d2cc11fa10698dc1937aff9720be27f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_2.8d2cc11f.png"
+  },
+  {
+    "revision": "afae2a39f412e36e9ee82856f973d9ce",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_3.afae2a39.png"
+  },
+  {
+    "revision": "d0bb69a49a80de7e002359c33bdc3c2b",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_4.d0bb69a4.png"
+  },
+  {
+    "revision": "374951137ebf5269433c2dd0f91a86a9",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_5.37495113.png"
+  },
+  {
+    "revision": "dcf7cd2a5cff1860aaa2451415503d90",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_6.dcf7cd2a.png"
+  },
+  {
+    "revision": "21afd4e8def368ac46d5f6d35df7214f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_7.21afd4e8.png"
+  },
+  {
+    "revision": "e4d753660f489043a105a449132a825f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_8.e4d75366.png"
+  },
+  {
+    "revision": "849e81c795763a25f760dd98c6a81832",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_9.849e81c7.png"
+  },
+  {
+    "revision": "7d81dd3d26ad3cee2e380455ffc9e6bc",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_prueba_1.7d81dd3d.png"
+  },
+  {
+    "revision": "b4f4c89eebd0a4f349b82bf048c6f413",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mapa_prueba_2.b4f4c89e.png"
+  },
+  {
+    "revision": "d11b65bb78e05fc6dd31e0aa628014d8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/marco.d11b65bb.png"
+  },
+  {
+    "revision": "f604e9c89f3dd146e4851aebca2e0ede",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/mist.f604e9c8.png"
+  },
+  {
+    "revision": "c2038015389f4a7dda072c21ff29d081",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-1.c2038015.png"
+  },
+  {
+    "revision": "fd4945f35fd2ddfef2d6034f89a20a7f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-10.fd4945f3.png"
+  },
+  {
+    "revision": "7b06397838853fb7886500f3f5a6a01d",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-2.7b063978.png"
+  },
+  {
+    "revision": "a2ee0fd6708985ee485c171ab4af92f6",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-3.a2ee0fd6.png"
+  },
+  {
+    "revision": "03e41fac8b858140ed69acf8c85d6805",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-4.03e41fac.png"
+  },
+  {
+    "revision": "167817f1368ac9c93dadb55d5826d2e2",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-5.167817f1.png"
+  },
+  {
+    "revision": "741c6ccf9f2372ff3fdbc119aac121a8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-6.741c6ccf.png"
+  },
+  {
+    "revision": "3fe118230b669bb8e7208ed7ffebe8c8",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-7.3fe11823.png"
+  },
+  {
+    "revision": "785d88aea78d7024d13deed243f3b306",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-8.785d88ae.png"
+  },
+  {
+    "revision": "1155afc434edf8af215016e81356261f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_-9.1155afc4.png"
+  },
+  {
+    "revision": "4de1ce92b22a83fa6be403fa744a4d8d",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_0.4de1ce92.png"
+  },
+  {
+    "revision": "d7d68e1ef70f71ceecc993bb1384bc68",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_1.d7d68e1e.png"
+  },
+  {
+    "revision": "1023e90bc672ecb5ec8dd1b3f66c172c",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_10.1023e90b.png"
+  },
+  {
+    "revision": "d75b3b81c611e4ef01cff2309ca55018",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_2.d75b3b81.png"
+  },
+  {
+    "revision": "83bc259e91ae8010697bd0a277947fe4",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_3.83bc259e.png"
+  },
+  {
+    "revision": "de8f37e5ef5988911f2105d9ce0bdc5f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_4.de8f37e5.png"
+  },
+  {
+    "revision": "b6c07c497ac79aa96ce966f3766b027f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_5.b6c07c49.png"
+  },
+  {
+    "revision": "5eb45cc35c50703f6dab658aa30cd368",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_6.5eb45cc3.png"
+  },
+  {
+    "revision": "79d0b4eaa896d30e9505db96a36eef8c",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_7.79d0b4ea.png"
+  },
+  {
+    "revision": "56acb3ebcd7d3ba2665cf47ec3f1fd61",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_8.56acb3eb.png"
+  },
+  {
+    "revision": "6d781354c0c1ab7d50724711b24d214f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/nivel_9.6d781354.png"
+  },
+  {
+    "revision": "abf5d192d85cad33c72e9a4ec342f00f",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/opener_menu.abf5d192.png"
+  },
+  {
+    "revision": "28db9160f9d6ca516009b1d02bf037df",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/restar_enemigos.28db9160.png"
+  },
+  {
+    "revision": "d7f89fa5e17c34a8aeadacaf9536cd7d",
+    "url": "https://emrivero.github.io/heimdall_eyes/static/media/sumar_enemigos.d7f89fa5.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f136beb2d358981b8eb76647e82647d2.js"
+  "https://emrivero.github.io/heimdall_eyes/precache-manifest.ab89fbb2e47616fa49ecb9df2e252aea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://emrivero.github.io/heimdall_eyes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
